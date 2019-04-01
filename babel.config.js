c99064924a5ad206bb97cc9cb9ecee2fec94d454
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2019-04-01 14:00:15 +0500

    add `babel.config.js` 👀

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+const babelConfig = api => {
+  api.cache.using(() => process.env.NODE_ENV);
+
+  const plugins = [];
+  const presets = ['@shaizei/babel-preset'];
+
+  return {
+    plugins,
+    presets,
+  };
+};
+
+module.exports = babelConfig;
